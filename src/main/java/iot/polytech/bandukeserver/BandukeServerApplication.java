d7b48a5c6eafced164ed6a060d902237969107ed
yohan <yohan.malbranque@gmail.com>
2020-11-23 08:31:14 +0100

    Initial commit

---
+++ src/main/java/iot/polytech/bandukeserver/BandukeServerApplication.java
@@ -0,0 +1,13 @@
+package iot.polytech.bandukeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandukeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BandukeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iot/polytech/bandukeserver/BandukeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package iot.polytech.bandukeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandukeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
